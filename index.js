0b82fbd5b294b8587f73c0dce077a21016d207f8
BinotaLIU <me@binota.org>
2020-05-12 17:10:58 +0800

    show diff url

--- index.js
+++ index.js
@@ -9,20 +9,28 @@ if (!/https:\/\/discord(app|)\.com\/api\/webhooks\/\d+?\/.+/i.exec(webhook)) {
   core.setFailed('The given discord webhook url is invalid. Please ensure you give a **full** url that start with "https://discordapp.com/api/webhooks"')
 }
 
+const shortSha = (i) => i.substr(0, 6)
+
 const escapeMd = (str) => str.replace(/([\[\]\\`\(\)])/g, '\\$1')
 
-const commits = github.context.payload.commits.map(i => ` - [\`[${i.id.substr(0, 6)}]\`](${i.url}) ${escapeMd(i.message)} - by ${i.author.name}`)
+const { payload: githubPayload } = github.context
+
+const commits = githubPayload.commits.map(i => ` - [\`[${shortSha(i.id)}]\`](${i.url}) ${escapeMd(i.message)} - by ${i.author.name}`)
 
 if (!commits.length) {
   return
 }
+
+const beforeSha = githubPayload.before
+const afterSha = githubPayload.after
+const compareUrl = `${githubPayload.repository.url}/compare/${beforeSha}...${afterSha}`
 
 const payload = {
   content: '',
   embeds: [
     {
       title: core.getInput('message-title') || 'Commits received',
-      description: commits.join('\n')
+      description: `[\`\[${shortSha(beforeSha)}...${shortSha(afterSha)}\]\`](${compareUrl})\n${commits.join('\n')}`
     }
   ]
 }
